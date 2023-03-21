cb8da4d21ea259a4d8c936c4b863d15b7cf0e68e
edRibeiro <edersonribeiro.ita@gmail.com>
2023-03-21 15:36:02 -0300

    Seção 3: CRUD

---
+++ src/user/user.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Patch } from "@nestjs/common";
+
+@Controller('users')
+export class UserController {
+    
+    @Post()
+    async create(@Body() body){
+        return {body};
+    }
+
+    @Get()
+    async read(){
+        return {users: []};
+    }
+    
+    @Get(':id')
+    async readOne(@Param() params){
+        return {user: {}, params};
+    }
+
+    @Put(':id')
+    async update(@Body() body, @Param() params){
+        return {
+            method: 'put',
+            body,
+            params
+        };
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() body, @Param() params){
+        return {
+            method: 'patch',
+            body,
+            params
+        };
+    }
+
+    @Delete(':id')
+    async delete(@Param() params){
+        return {
+            params
+        };
+    }
+}
