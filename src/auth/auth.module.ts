25ae384e542f61554e7107c2870bdeaca9b5dc74
edRibeiro <edersonribeiro.ita@gmail.com>
2023-05-22 17:59:15 -0300

    Seção 7: Segurança - Autenticação. Aula 40: Verificar Tokens JWT.

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module, forwardRef } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { UserModule } from 'src/user/user.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthService } from './auth.service';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: `322!6drKhQ%J7(DueShSPL^AqT*UEkys`,
+    }),
+    forwardRef(() => UserModule),
+    PrismaModule,
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,20 +1,26 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
-import { PrismaModule } from "src/prisma/prisma.module";
-import { UserController } from "./user.controller";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { UserIdCheckMiddleware } from "src/middlewares/user-id-check.middleware";
+import { UserIdCheckMiddleware } from 'src/middlewares/user-id-check.middleware';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-    imports: [PrismaModule],
-    controllers: [UserController],
-    providers: [UserService],
-    exports: []
-  })
-  export class UserModule implements NestModule {
-    configure(consumer: MiddlewareConsumer) {
-      consumer.apply(UserIdCheckMiddleware).forRoutes({
-        path: 'users/:id',
-        method: RequestMethod.ALL
-      });
-    }
+  imports: [PrismaModule, AuthModule],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
+})
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(UserIdCheckMiddleware).forRoutes({
+      path: 'users/:id',
+      method: RequestMethod.ALL,
+    });
   }
+}
